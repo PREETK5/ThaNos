ba0bd5c5aacaab450b6851ae0f9442a414a1390e
PREETK5 <50269094+PREETK5@users.noreply.github.com>
2019-09-08 14:13:33 +0530

    .

--- IHbot/modules/translations/English.py
+++ IHbot/modules/translations/English.py
@@ -204,7 +204,7 @@ EnglishStrings = {
 I'm a group manager bot maintained by [this person](tg://user?id={654839744}). I'm a [Marie](https://github.com/PaulSonOfLars/tgbot) fork.
 I'm built in python3, using the \
 python-telegram-bot library, and am fully opensource - you can find what makes me tick\
-[here](https://github.com/peaktogoo/HarukaAya))!
+[here](https://github.com/peaktogoo/IHbotAya))!
 
 Feel free to submit pull requests on github, or to contact my support group, @EchelonTeam, with any bugs, questions \
 or feature requests you might have :)
